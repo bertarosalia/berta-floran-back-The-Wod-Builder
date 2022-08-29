d453a6d2011ffbd2c3f8b54a12aca94a19e23e22
Berta Floran <bertafloran@hotmail.com>
2022-08-29 22:35:57 +0200

    create startServer file

---
+++ src/server/startServer.ts
@@ -0,0 +1,25 @@
+import "../loadEnvironment";
+import chalk from "chalk";
+import Debug from "debug";
+import app from ".";
+import CustomError from "../utils/CustomError";
+
+const debug = Debug("the-wod-builder-back:server:startServer");
+
+const startServer = (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.green(`Server listening on http://localhost:${port}`));
+      resolve(true);
+    });
+
+    server.on("error", (error: CustomError) => {
+      debug(chalk.red("Error starting the server"));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`Port ${port} in use`));
+      }
+      reject(error);
+    });
+  });
+
+export default startServer;
