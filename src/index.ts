b64e286ded44c856ff7264ca6bccb82677682885
Berta Floran <bertafloran@hotmail.com>
2022-08-29 23:30:16 +0200

    connect with mongo database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import connectDB from "./database";
+import "./loadEnvironment";
+import startServer from "./server/startServer";
 
+const port = +process.env.PORT || 4000;
+const mongoURL = process.env.MONGODB_URL;
+
+(async () => {
+  try {
+    await connectDB(mongoURL);
+    await startServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
