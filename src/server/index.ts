0da7616b1a035080087672ff5c9d48eeb80b7848
Berta Floran <bertafloran@hotmail.com>
2022-08-30 16:26:48 +0200

    add general and not found errors to middlewares

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { generalError, notFoundError } from "./middlewares/errors";
 
 const app = express();
+app.disable("x-powered-by");
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
