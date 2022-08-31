388e83f5ea47cd609e6aa5d493a908de235f887b
Berta Floran <bertafloran@hotmail.com>
2022-08-31 16:11:25 +0200

    add the wod builder router to index

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { generalError, notFoundError } from "./middlewares/errors";
+import theWodBuilderRouter from "./routers/theWodBuilderRouter";
 
 const app = express();
 app.disable("x-powered-by");
@@ -9,6 +10,8 @@ app.disable("x-powered-by");
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/theWodBuilder", theWodBuilderRouter);
 
 app.use(notFoundError);
 app.use(generalError);
