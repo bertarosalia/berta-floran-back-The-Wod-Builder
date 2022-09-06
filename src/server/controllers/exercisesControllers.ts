35a41d4adb8124f5ab7e7f5eb362006843889cf7
Berta Floran <bertafloran@hotmail.com>
2022-09-06 10:03:46 +0200

    create delete exercise controller

--- src/server/controllers/exercisesControllers.ts
+++ src/server/controllers/exercisesControllers.ts
@@ -1,10 +1,35 @@
-import { Request, Response } from "express";
+import Debug from "debug";
+import { NextFunction, Request, Response } from "express";
 import Exercise from "../../database/models/Exercise";
+import CustomError from "../../utils/CustomError";
 
-const getExercises = async (req: Request, res: Response) => {
+const debug = Debug("the-wod-builder:database:index");
+
+export const getExercises = async (req: Request, res: Response) => {
   const exercises = await Exercise.find();
 
   res.status(200).json({ exercises });
 };
 
-export default getExercises;
+export const deleteExercise = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const idExercise = req.params.id;
+  debug("Trying to delete exercise");
+
+  try {
+    await Exercise.findByIdAndDelete(idExercise);
+    res.status(200).json({ message: "Successfully deleted exercise" });
+
+    debug("Received a request to delete an exercise");
+  } catch (error) {
+    const mongooseError = new CustomError(
+      404,
+      "No exercises found",
+      "Error deleting exercise"
+    );
+    next(mongooseError);
+  }
+};
