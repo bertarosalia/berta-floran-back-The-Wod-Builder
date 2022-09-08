af25d925b442b9d4536bdde3504a4844cb2ed43e
Berta Floran <bertafloran@hotmail.com>
2022-09-09 01:18:16 +0200

    rename TWBRouter to exercisesrouter and add usersrouter to index

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { generalError, notFoundError } from "./middlewares/errors";
-import theWodBuilderRouter from "./routers/theWodBuilderRouter";
+import theWodBuilderRouter from "./routers/exercisesRouter/exercisesRouter";
+import usersRouter from "./routers/usersRouter/usersRouter";
 
 const app = express();
 app.disable("x-powered-by");
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("users", usersRouter);
 app.use("/theWodBuilder", theWodBuilderRouter);
 
 app.use(notFoundError);
