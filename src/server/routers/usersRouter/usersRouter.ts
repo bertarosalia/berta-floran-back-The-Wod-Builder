8b30789481730cb5b2aa859cd398625ab18605d2
Berta Floran <bertafloran@hotmail.com>
2022-09-09 10:51:16 +0200

    add register validation to users router

--- src/server/routers/usersRouter/usersRouter.ts
+++ src/server/routers/usersRouter/usersRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { validate } from "express-validation";
 import registerUser from "../../controllers/UsersControllers/usersController";
+import registerValidation from "../../schemas/usersCredentialsSchema";
 
 const usersRouter = express.Router();
 
-usersRouter.post("/register", registerUser);
+usersRouter.post("/register", validate(registerValidation), registerUser);
 
 export default registerUser;
