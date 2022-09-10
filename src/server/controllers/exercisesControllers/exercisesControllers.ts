6795acf9e9c9e2ea508df47af0135b22d5a64e7a
Berta Floran <bertafloran@hotmail.com>
2022-09-10 14:56:26 +0200

    refactorize exercises controllers

--- src/server/controllers/exercisesControllers/exercisesControllers.ts
+++ src/server/controllers/exercisesControllers/exercisesControllers.ts
@@ -14,26 +14,15 @@ export const getExercises = async (
 
   try {
     exercises = await Exercise.find();
-
-    if (exercises.length === 0) {
-      const errorExercise = new CustomError(
-        404,
-        "No exercises in database",
-        "Exercises not found"
-      );
-      next(errorExercise);
-      return;
-    }
+    res.status(200).json({ exercises });
   } catch (error) {
-    const mongooseError = new CustomError(
+    const errorExercise = new CustomError(
       404,
-      error.message,
-      "Error getting exercises"
+      "No exercises in database",
+      "Exercises not found"
     );
-    next(mongooseError);
-    return;
+    next(errorExercise);
   }
-  res.status(200).json({ exercises });
 };
 
 export const deleteExercise = async (
@@ -59,26 +48,5 @@ export const deleteExercise = async (
       "Error deleting exercise"
     );
     next(newError);
-  }
-};
-
-export const createExercise = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const newExercise = req.body;
-
-  try {
-    const newExerciseCreated = await Exercise.create(newExercise);
-    res.status(201).json({ exercise: newExerciseCreated });
-  } catch (error) {
-    const customError = new CustomError(
-      400,
-      error.message,
-      "Error creating new exercise"
-    );
-
-    next(customError);
   }
 };
