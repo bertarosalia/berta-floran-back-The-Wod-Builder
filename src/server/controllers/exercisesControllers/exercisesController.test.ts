66aab0ac209259c084f692bbd36780e7a29bc626
Berta Floran <bertafloran@hotmail.com>
2022-09-10 14:56:54 +0200

    fix tests exercises controllers

--- src/server/controllers/exercisesControllers/exercisesController.test.ts
+++ src/server/controllers/exercisesControllers/exercisesController.test.ts
@@ -4,7 +4,7 @@ import CustomError from "../../../utils/CustomError";
 import { deleteExercise, getExercises } from "./exercisesControllers";
 
 let res: Partial<Response>;
-let next: Partial<NextFunction>;
+let next = jest.fn() as NextFunction;
 
 beforeAll(() => {
   res = {
@@ -18,6 +18,18 @@ afterAll(() => {
   jest.clearAllMocks();
 });
 
+jest.mock("../../../database/models/Exercise", () => ({
+  find: jest.fn().mockReturnValue([
+    {
+      id: "2151",
+      body: "legs",
+      name: "back squat",
+      description: "",
+      image: "http",
+    },
+  ]),
+}));
+
 describe("Given a controller get all exercises", () => {
   describe("When it receives a request", () => {
     const req: Partial<Request> = {};
@@ -30,24 +42,19 @@ describe("Given a controller get all exercises", () => {
 
       expect(res.status).toHaveBeenCalledWith(200);
     });
-    describe("And database return a void list of exercises", () => {
+    describe("When called but doesn´t return any valid data", () => {
       test("Then call next function with an error", async () => {
-        const errorExercise = new CustomError(
+        Exercise.find = jest.fn().mockRejectedValue(new Error());
+
+        const expectedError = new CustomError(
           404,
           "No exercises in database",
           "Exercises not found"
         );
 
-        const mockExerciseList: void[] = [];
-        Exercise.find = jest.fn().mockResolvedValue(mockExerciseList);
+        await getExercises(req as Request, res as Response, next);
 
-        await getExercises(
-          req as Request,
-          res as Response,
-          next as NextFunction
-        );
-
-        expect(next).toHaveBeenCalledWith(errorExercise);
+        expect(next).toHaveBeenCalledWith(expectedError);
       });
     });
   });
@@ -73,39 +80,61 @@ describe("Given a controller delete one exercise by id", () => {
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalledWith(expectedJsonMessage);
     });
-  });
-  describe("When it receives a request to delete an item but can´t find it", () => {
-    test("It should throw a custom error with 404 as code", async () => {
-      const requestExample = {
-        params: { id: "" },
-      } as Partial<Request>;
+    describe("When it receives a request to delete an item but can´t find it", () => {
+      test("It should throw a custom error with 404 as code", async () => {
+        const requestExample = {
+          params: { id: "" },
+        } as Partial<Request>;
 
-      const expectedError = new CustomError(
-        404,
-        "No exercises found",
-        "Error deleting exercise"
-      );
+        const expectedError = new CustomError(
+          404,
+          "No exercises found",
+          "Error deleting exercise"
+        );
 
-      Exercise.findByIdAndDelete = jest.fn().mockRejectedValue(expectedError);
+        Exercise.findByIdAndDelete = jest.fn().mockRejectedValue(expectedError);
 
-      const responseExample = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      } as Partial<Response>;
+        const responseExample = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        } as Partial<Response>;
 
-      await deleteExercise(
-        requestExample as Request,
-        responseExample as Response,
-        next as NextFunction
-      );
+        await deleteExercise(
+          requestExample as Request,
+          responseExample as Response,
+          next as NextFunction
+        );
 
-      expect(next).toHaveBeenCalledWith(expectedError);
+        expect(next).toHaveBeenCalledWith(expectedError);
+      });
+      describe("When it receives a request to delete an exercise, but the value is null", () => {
+        test("Then it should send a response with the method status 404", async () => {
+          const requestTest = {
+            params: { id: "515165" },
+          } as Partial<Request>;
+
+          const expectedResult: void = null;
+
+          Exercise.findByIdAndDelete = jest
+            .fn()
+            .mockResolvedValue(expectedResult);
+
+          const expectedStatus = 404;
+
+          const responseTest = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis(),
+          } as Partial<Response>;
+
+          await deleteExercise(
+            requestTest as Request,
+            responseTest as Response,
+            next
+          );
+
+          expect(responseTest.status).toHaveBeenCalledWith(expectedStatus);
+        });
+      });
     });
-  });
-});
-
-describe("Given a create exercise controller", () => {
-  describe("When it´s called with an exercises as request", () => {
-    test("It should response with an exercise created", () => {});
   });
 });
