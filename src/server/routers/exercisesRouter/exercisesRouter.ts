c822f0697ce3578ca8a21564b26a33fc82eb2a4a
Berta Floran <bertafloran@hotmail.com>
2022-09-11 14:44:01 +0200

    add get by id exercises to router

--- src/server/routers/exercisesRouter/exercisesRouter.ts
+++ src/server/routers/exercisesRouter/exercisesRouter.ts
@@ -3,6 +3,7 @@ import {
   getExercises,
   deleteExercise,
   createExercise,
+  getById,
 } from "../../controllers/exercisesControllers/exercisesControllers";
 
 const theWodBuilderRouter = express.Router();
@@ -10,5 +11,6 @@ const theWodBuilderRouter = express.Router();
 theWodBuilderRouter.get("/exercises", getExercises);
 theWodBuilderRouter.delete("/:id", deleteExercise);
 theWodBuilderRouter.post("/create", createExercise);
+theWodBuilderRouter.get("/:id", getById);
 
 export default theWodBuilderRouter;
