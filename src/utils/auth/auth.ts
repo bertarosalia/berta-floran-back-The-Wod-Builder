54132c66278818ade020606470e6212aeaabd0aa
Berta Floran <bertafloran@hotmail.com>
2022-09-23 00:06:53 +0200

    add create token to auth

--- src/utils/auth/auth.ts
+++ src/utils/auth/auth.ts
@@ -1,4 +1,11 @@
+import "../../loadEnvironment";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import CustomJwtPayload from "../../types/payload";
+
+export const createToken = (payload: CustomJwtPayload) => {
+  jwt.sign(payload, process.env.SECRET);
+};
 
 export const hashCreator = (text: string) => {
   const salt = 10;
