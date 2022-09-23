5e2e7b30111de8695cbdb54189e43143bd70c776
Berta Floran <bertafloran@hotmail.com>
2022-09-23 14:26:41 +0200

    add user login to user router

--- src/server/routers/usersRouter/usersRouter.ts
+++ src/server/routers/usersRouter/usersRouter.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { validate } from "express-validation";
-import userRegister from "../../controllers/UsersControllers/usersController";
+import {
+  userLogin,
+  userRegister,
+} from "../../controllers/UsersControllers/usersController";
 import registerValidation from "../../schemas/usersCredentialsSchema";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", validate(registerValidation), userRegister);
+userRouter.post("/login", userLogin);
 
 export default userRegister;
