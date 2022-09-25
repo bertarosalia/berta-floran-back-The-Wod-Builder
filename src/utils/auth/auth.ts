57545b8475aae699ad14c9f5e65bce979075db07
Berta Floran <bertafloran@hotmail.com>
2022-09-25 23:45:50 +0200

    refactor auth to fix user login

--- src/utils/auth/auth.ts
+++ src/utils/auth/auth.ts
@@ -3,9 +3,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import CustomJwtPayload from "../../types/payload";
 
-export const createToken = (payload: CustomJwtPayload) => {
+export const createToken = (payload: CustomJwtPayload) =>
   jwt.sign(payload, process.env.SECRET);
-};
 
 export const hashCreator = (text: string) => {
   const salt = 10;
@@ -13,6 +12,5 @@ export const hashCreator = (text: string) => {
   return bcrypt.hash(text, salt);
 };
 
-export const hashCompare = (text: string, hash: string) => {
+export const hashCompare = (text: string, hash: string) =>
   bcrypt.compare(text, hash);
-};
