534e3ec163e1bb322e2d2c730f17cca0a6853c52
Berta Floran <bertafloran@hotmail.com>
2022-09-26 17:09:00 +0200

    remove validation from register router

--- src/server/routers/usersRouter/usersRouter.ts
+++ src/server/routers/usersRouter/usersRouter.ts
@@ -1,14 +1,13 @@
 import express from "express";
-// import { validate } from "express-validation";
+
 import {
   userLogin,
   userRegister,
 } from "../../controllers/UsersControllers/usersController";
-// import registerValidation from "../../schemas/usersCredentialsSchema";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userRegister);
+userRouter.post("/register",  userRegister);
 userRouter.post("/login", userLogin);
 
 export default userRouter;
