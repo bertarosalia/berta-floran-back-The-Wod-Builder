9331173c8d902f2254ad6da51e1476fc9911bf84
Berta Floran <bertafloran@hotmail.com>
2022-09-27 16:05:10 +0200

    create mock exercise for testing

---
+++ src/test-utils/mocks/mockExercise.ts
@@ -0,0 +1,11 @@
+const mockExercise = [
+  {
+    id: "1254485",
+    body: "body part",
+    name: "exercise name",
+    description: "what to do",
+    image: "how to do",
+  },
+];
+
+export default mockExercise;
